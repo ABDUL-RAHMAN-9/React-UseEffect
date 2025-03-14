885dd32f92d0ad837e6fc2e6f7dcf5c30229a6c8
Abdul Rahman <abdulrahman161004@gmail.com>
2025-03-14 22:44:51 +0530

    Add Timer component with useState and useEffect for rendering updates & style it

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
-function Timer() {
-  return (
-    <div>
-      
-    </div>
-  )
+function Timer()
+{
+    const [count, setCount] = useState(0);
+
+    useEffect(() =>
+    {
+        setTimeout(() =>
+        {
+            setCount((count) => count + 1);
+        }, 1000);
+    });
+
+    return (
+        <div className='container'>
+            <h1>I've rendered {count} times!</h1>
+        </div>
+    )
 }
 
 export default Timer
